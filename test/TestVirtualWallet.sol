9e96154e499a719a220738630053d9f98442ab88
ethdev <eth@eth.eth>
2018-04-14 18:46:08 +0200

    dummy first test

---
+++ test/TestVirtualWallet.sol
@@ -0,0 +1,21 @@
+pragma solidity 0.4.21;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+
+import "../contracts/VirtualWallet.sol";
+
+
+contract TestVirtualWallet {
+
+    function testInitialBalanceOfWallet() {
+
+        VirtualWallet testWallet = VirtualWallet(DeployedAddresses.MetaCoin());
+
+        uint expected = 0;
+
+        Assert.equal(testWallet.getBalance(tx.origin), expected, "Waller should have 0 units of basic token");
+
+    }
+
+}
