ed532b95d196d5307986edff962acec06d2decad
Guziec-Lobuziec <eth@eth.eth>
2018-04-17 18:04:27 +0200

    IS7 test - testing basic agreement prop.

---
+++ test/TestAgreement.sol
@@ -0,0 +1,24 @@
+pragma solidity 0.4.21;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+
+import "../contracts/AgreementManager.sol";
+
+
+contract TestAgreement {
+
+    function testAgreementDefaultProperties() {
+
+        AgreementManager testManager = AgreementManager(DeployedAddresses.AgreementManager());
+
+        Agreement testAgreement = Agreement(testManager.create());
+
+        Assert.equal(testAgreement.getParticipants(), this, "Test should be creator and only participan");
+        Assert.equal(testAgreement.getCreationBlock(), block.number, "Should be created in same block as test tx");
+        Assert.equal(testAgreement.getCreationTimestamp(), block.timestamp, "Same timestamp as in test");
+        Assert.equal(testAgreement.getStatus(), Agreement.Status.New, "Should have \"New\" Status");
+
+    }
+
+}
