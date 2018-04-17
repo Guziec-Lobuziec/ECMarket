a594f39c501fc2b2f5571428ff17dbb4e0bd4111
Guziec-Lobuziec <eth@eth.eth>
2018-04-17 18:31:56 +0200

    IS7 code - adding view functions to Agreement

--- contracts/Agreement.sol
+++ contracts/Agreement.sol
@@ -2,5 +2,31 @@ pragma solidity 0.4.21;
 
 
 contract Agreement {
+    enum Status { New }
+    address private creator;
+    uint private creationBlock;
+    uint private creationTimestamp;
+
+    function Agreement(address _creator) public {
+        creator = _creator;
+        creationBlock = block.number;
+        creationTimestamp = block.timestamp;
+    }
+
+    function getParticipants() public view returns(address) {
+        return creator;
+    }
+
+    function getCreationBlock() public view returns(uint) {
+        return creationBlock;
+    }
+
+    function getCreationTimestamp() public view returns(uint) {
+        return creationTimestamp;
+    }
+
+    function getStatus() public view returns(Status) {
+        return Status.New;
+    }
 
 }
