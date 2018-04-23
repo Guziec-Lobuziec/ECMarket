c38f3bcf53021af1112ef2cc1c55beba78d73d54
Kolejarzyk <pawel.dee@live.com>
2018-04-23 19:57:29 +0200

    getBalance implementation, testPayInBalanceOfWallet test

--- contracts/VirtualWallet.sol
+++ contracts/VirtualWallet.sol
@@ -3,8 +3,15 @@ pragma solidity 0.4.21;
 
 contract VirtualWallet {
 
-    function getBalance(address externalWallet) public view returns (uint balance) {
-        return 0;
+
+    function getBalance() public view returns (uint balance) {
+        return address(this).balance;
     }
 
+
+
+    function payIn(address _from, uint amount) public payable
+    {
+        _from.transfer(amount);
+    }
 }
