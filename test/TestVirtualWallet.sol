2450523c54f47b9d24a721981c860e345fec14f9
Kolejarzyk <pawel.dee@live.com>
2018-04-23 19:57:29 +0200

    getBalance implementation, testPayInBalanceOfWallet test

--- test/TestVirtualWallet.sol
+++ test/TestVirtualWallet.sol
@@ -10,22 +10,23 @@ contract TestVirtualWallet {
 
     function testInitialBalanceOfWallet() {
 
-        VirtualWallet testWallet = VirtualWallet(DeployedAddresses.VirtualWallet());
+        VirtualWallet testWallet = new VirtualWallet();
 
         uint expected = 0;
 
-        Assert.equal(testWallet.getBalance(tx.origin), expected, "Waller should have 0 units of basic token");
+        Assert.equal(testWallet.getBalance(), expected, "Waller should have 0 units of basic token");
 
     }
 
     function testPayInBalanceOfWallet(){
 
-        uint public etherBalance = 3 ether;
-        VirtualWallet testWallet = VirtualWallet(DeployedAddresses.VirtualWallet());
+        uint etherBalance = 3 ether; 
+        VirtualWallet testWallet = new VirtualWallet();
 
-        address public _from;
-        testWallet.payIn(_from,testWallet.getAddress,etherBalance)
-        Assert.equal(testWallet.getBalance(tx.origin),3, "Wallet should have 3 units of basic token");
+
+        VirtualWallet testWallet1 = new VirtualWallet();
+        testWallet.payIn(address(testWallet1),etherBalance);
+        Assert.equal(testWallet.getBalance(),3, "Wallet should have 3 units of basic token");
     }
 
 }
