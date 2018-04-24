04c128ce1202f452385d3f59f48b38b2a88322bf
Kolejarzyk <pawel.dee@live.com>
2018-04-24 12:50:13 +0200

    IS2 Test multi users pay_in pay_out

--- test/TestVirtualWallet.js
+++ test/TestVirtualWallet.js
@@ -35,3 +35,58 @@ contract("VirtualWallet payin and payout test", async (accounts) => {
         );
     })
 })
+
+contract("VirtualWallet multiple users test", async (accounts) => {
+    it("multiple payins", async () => {
+        let testWallet = await VirtualWallet.deployed();
+        let testValue1 = 1000;
+        let testValue2 = 2000;
+
+        let before1 = await testWallet.getBalance.call(accounts[0]);
+        assert.equal(before1.toNumber(),0, "Should be 0");
+
+        let before2 = await testWallet.getBalance.call(accounts[1]);
+        assert.equal(before2.toNumber(),0, "Should be 0");
+
+        await testWallet.payIn({from: accounts[0], value: testValue1});
+        await testWallet.payIn({from: accounts[1], value: testValue2});
+
+        let after1 = await testWallet.getBalance.call(accounts[0]);
+        assert.equal(after1.toNumber(),testValue1, "Should be "+testValue1);
+
+        let after2 = await testWallet.getBalance.call(accounts[1]);
+        assert.equal(after2.toNumber(),testValue2, "Should be "+testValue2);
+
+    })
+
+    it("multiple payouts", async () => {
+        let testWallet = await VirtualWallet.deployed();
+        let testValue1 = 1000;
+        let testValue2 = 2000;
+        let payout1 = 400;
+        let payout2 = 1200;
+
+        let before1 = await testWallet.getBalance.call(accounts[0]);
+        assert.equal(before1.toNumber(),testValue1, "Should be "+testValue1);
+
+        let before2 = await testWallet.getBalance.call(accounts[1]);
+        assert.equal(before2.toNumber(),testValue2, "Should be "+testValue2);
+
+        await testWallet.payOut(payout2,{from: accounts[1]});
+        await testWallet.payOut(payout1,{from: accounts[0]});
+
+        let after1 = await testWallet.getBalance.call(accounts[0]);
+        assert.equal(
+            after1.toNumber(),
+            testValue1-payout1,
+            "Should be "+(testValue1-payout1)
+        );
+
+        let after2 = await testWallet.getBalance.call(accounts[1]);
+        assert.equal(
+            after2.toNumber(),
+            testValue2-payout2,
+            "Should be "+(testValue2-payout2)
+        );
+    })
+})
