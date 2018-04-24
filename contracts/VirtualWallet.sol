88590808f8b387d9fde191399f43ce4acdcd8c0d
Kolejarzyk <pawel.dee@live.com>
2018-04-24 11:38:05 +0200

    IS1 Test payin implementation

--- contracts/VirtualWallet.sol
+++ contracts/VirtualWallet.sol
@@ -3,15 +3,16 @@ pragma solidity 0.4.21;
 
 contract VirtualWallet {
 
+    uint private value;
 
     function getBalance(address externalWallet) public view returns (uint balance) {
-        return 0;
+        return value;
     }
 
 
 
     function payIn() public payable
     {
-    
+        value = msg.value;    
     }
 }
