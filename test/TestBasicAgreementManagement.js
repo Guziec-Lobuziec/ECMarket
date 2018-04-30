9e5da36d65fd1fbe4a4ee1b4f5ae853819bdab4f
Guziec-Lobuziec <eth@eth.eth>
2018-04-30 09:22:50 +0200

    refactor TestBasicAgreementManagement.js

--- test/TestBasicAgreementManagement.js
+++ test/TestBasicAgreementManagement.js
@@ -1,6 +1,6 @@
 const {assertRevert} = require('./helpers/assertThrow');
-const AgreementManager = artifacts.require("AgreementManager");
-const Agreement = artifacts.require("Agreement");
+const AgreementManager = artifacts.require('AgreementManager');
+const Agreement = artifacts.require('Agreement');
 
 async function createManyAgreements(manager, setupData) {
 
@@ -14,59 +14,87 @@ async function createManyAgreements(manager, setupData) {
         return Promise.all(transactions);
 }
 
-contract("Agreement basic management - removal", async (accounts) => {
+contract('Agreement basic management - creation, removal', async (accounts) => {
 
-  it("Test if agreement did selfdestruction", async () => {
+  let testManager;
+  let createTransactions = [];
+  let agreementAddress = {};
 
-    let testManager = await AgreementManager.deployed();
+  before(async () => {
+    testManager = await AgreementManager.deployed();
+  })
 
-    let before = await testManager.search();
+  it('Agreement creation events', async () => {
+
+    let before = await testManager.search.call();
     assert.isTrue(before.every((e) => {return e == 0;}),'expected to be zeros before');
 
-    let createTransactions = await createManyAgreements(testManager, [{address: accounts[0], count: 1}]);
+    createTransactions = await createManyAgreements(testManager, [{address: accounts[0], count: 1}]);
 
-    assert.equal(createTransactions[0].logs.length, 1, "one event generated");
-    assert.equal(createTransactions[0].logs[0].event, "AgreementCreation", "event name");
+    assert.equal(createTransactions[0].logs.length, 1, 'one event generated');
+    assert.equal(createTransactions[0].logs[0].event, 'AgreementCreation', 'event name');
 
-    let agreementAddress = createTransactions[0].logs[0].args.created;
+  })
+
+  it('Checking if contract exists under given address', async () => {
+
+    agreementAddress = createTransactions[0].logs[0].args.created;
 
     console.log(agreementAddress);
 
     assert.notEqual(agreementAddress, 0, 'should have valid address');
 
     let codeOfAgreementBefore = await web3.eth.getCode(agreementAddress);
-    assert.notEqual(codeOfAgreementBefore, "0x0", "should have some code");
+    assert.notEqual(codeOfAgreementBefore, '0x0', 'should have some code');
 
-    let agreements = await testManager.search();
-    let one = agreements.filter((e) => {return e != 0;});
+  })
+
+  it('Checking if search results match events logs', async () => {
+
+    let one = (await testManager.search.call()).filter((e) => {return e != 0;});
     assert.lengthOf(one, 1,'exactly one non zero');
-    assert.equal(one[0], agreementAddress, "manager should return the same address");
+    assert.equal(one[0], agreementAddress, 'manager should return the same address');
 
+  })
+
+  it('Test if agreement did selfdestruction', async () => {
     let agreement = await Agreement.at(agreementAddress);
     await agreement.remove({from: accounts[0]});
 
     let codeOfAgreementAfter = await web3.eth.getCode(agreementAddress);
-    assert.equal(codeOfAgreementAfter, "0x0", "should have none");
+    assert.equal(codeOfAgreementAfter, '0x0', 'should have none');
 
-    let after = await testManager.search();
+    let after = await testManager.search.call();
     assert.isTrue(after.every((e) => {return e == 0;}),'expected to be zeros after');
 
   })
 
-})
+});
 
-contract("Agreement basic management - remove selected", async (accounts) => {
+contract('Agreement basic management - remove selected', async (accounts) => {
 
-  it("Test if only selected agreement is removed", async () => {
-    let testManager = await AgreementManager.deployed();
+  let testManager;
+  let agreements;
 
-    let before = await testManager.search();
-    assert.isTrue(before.every((e) => {return e == 0;}),'expected to be zeros before');
+  before(async () => {
+    testManager = await AgreementManager.deployed();
+  })
+
+  it('Checking if two agreements were created', async () => {
+
+    assert.isTrue(
+      (await testManager.search.call()).every((e) => {return e == 0;}),
+      'expected to be zeros before'
+    );
 
     await createManyAgreements(testManager, [{address: accounts[0], count: 2}]);
 
-    let agreements = await testManager.search();
-    assert.equal(agreements.filter((e) => {return e != 0;}).length, 2, "Should be two non zero records");
+    agreements = await testManager.search.call();
+    assert.equal(agreements.filter((e) => {return e != 0;}).length, 2, 'Should be two non zero records');
+
+  })
+
+  it('Check if event logs and search results match', async () => {
 
     let creationLogs = await (new Promise(function(resolve,reject) {
       testManager.AgreementCreation({},{fromBlock: 0, toBlock: 'latest'})
@@ -79,29 +107,40 @@ contract("Agreement basic management - remove selected", async (accounts) => {
     }));
 
     console.log(creationLogs.map((l) => {return l.args.created}));
-    assert.equal(creationLogs.length, 2, "Should be two events");
+    assert.equal(creationLogs.length, 2, 'Should be two events');
     assert.include(
       agreements.toString(),
       creationLogs.map((l) => {return l.args.created}),
-      "Search and logs should match"
+      'Search and logs should match'
     );
+
+  })
+
+  it('Test if only selected agreement is removed', async () => {
 
     let createdAgreements = agreements.filter((e) => {return e != 0;});
     let agreementToBeRemoved = await Agreement.at(createdAgreements[0]);
     await agreementToBeRemoved.remove({from: accounts[0]});
 
-    let after = await testManager.search();
+    let after = await testManager.search.call();
     assert.notInclude(after, createdAgreements[0],'no longer exists');
     assert.include(after, createdAgreements[1], 'second agreement still tracked');
-    assert.equal(await web3.eth.getCode(createdAgreements[0]), '0x0', "destroyed");
-    assert.notEqual(await web3.eth.getCode(createdAgreements[1]), '0x0', "untouched");
+    assert.equal(await web3.eth.getCode(createdAgreements[0]), '0x0', 'destroyed');
+    assert.notEqual(await web3.eth.getCode(createdAgreements[1]), '0x0', 'untouched');
 
   })
 })
 
-contract("Agreement basic management - permissions to remove", async (accounts) => {
-  it("Test if only creator can remove agreement", async () => {
-    let testManager = await AgreementManager.deployed();
+contract('Agreement basic management - permissions to remove', async (accounts) => {
+
+  let testManager;
+  let agreementsAddresses;
+
+  before(async () => {
+    testManager = await AgreementManager.deployed();
+  })
+
+  it('Check if correctly created', async () => {
 
     let before = await testManager.search.call();
     assert.isTrue(before.every((e) => {return e == 0;}),'expected to be zeros before');
@@ -111,18 +150,22 @@ contract("Agreement basic management - permissions to remove", async (accounts) 
       [{address: accounts[0], count: 2},{address: accounts[1], count: 1}]
     );
 
-    let agreementsAddresses = (await testManager.search.call()).filter((e) => {return e != 0;});
-    assert.equal(agreementsAddresses.length, 3, "Should be three non zero records");
+    agreementsAddresses = (await testManager.search.call()).filter((e) => {return e != 0;});
+    assert.equal(agreementsAddresses.length, 3, 'Should be three non zero records');
+
+  })
+
+  it('Test if only creator can remove agreement', async () => {
 
     let agreements = await Promise.all(agreementsAddresses.map((e) => {return Agreement.at(e);}));
 
-    await assertRevert(agreements[2].remove({from: accounts[0]}),"3rd should revert");
-    await assertRevert(agreements[0].remove({from: accounts[1]}),"1st should revert");
+    await assertRevert(agreements[2].remove({from: accounts[0]}),'3rd should revert');
+    await assertRevert(agreements[0].remove({from: accounts[1]}),'1st should revert');
 
     assert.equal(
       (await testManager.search.call()).filter((e) => {return e != 0;}).length,
       3,
-      "Should be three non zero records"
+      'Should be three non zero records'
     );
   })
 })
