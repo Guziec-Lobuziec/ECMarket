dd5bfd8e5fba0915f391bb5f3e026f08f4d13317
hentaininja <hasamt94@gmail.com>
2018-05-10 17:13:47 +0200

    rating: language test commit

---
+++ test/TestRatingSystem.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertEquals;
+
+public class TestRatingSystem {
+    
+    public void addRatingTest() {
+
+        rank = 1;
+
+        assertEquals(1, rank);
+    }
+
+}
