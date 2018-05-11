c17a06213c852408a1191edc09e10cd359f7ccdb
Avarice111 <galyn43@gmail.com>
2018-05-11 14:49:22 +0200

    IS14 test - Name

---
+++ test/TestAdvancedProperties.js
@@ -0,0 +1,22 @@
+const {createManyAgreements} = require('./helpers/agreementFactory');
+const {assertRevert} = require('./helpers/assertThrow');
+const AgreementManager = artifacts.require('AgreementManager');
+const Agreement = artifacts.require('Agreement');
+
+contract('Test Advanced Properties', async (accounts) => {
+    let testManager;
+
+    before(async () => {
+        testManager = await AgreementManager.deployed();
+      })
+
+    it('Agreement returns name', async () => {
+        let transaction = await testManager.create("TestName", {from: accounts[0]});
+        let agreementAdress = transaction.logs[0].args.created;
+        let agreement = await Agreement.at(agreementAddress);
+        let name = await agreement.getName.call();
+        assert.equal(name, "TestName", "Agreement doesn't return Name");
+
+    })
+    
+})
