49451f9abda2a9724de7e5c4453984feac74f6f1
hentaininja <hasamt94@gmail.com>
2018-05-23 09:30:57 +0200

    IS3 code - return value fix

--- contracts/VirtualWallet.sol
+++ contracts/VirtualWallet.sol
@@ -4,6 +4,7 @@ pragma solidity 0.4.23;
 contract VirtualWallet {
 
     mapping (address => uint) walletValue;
+    mapping (address => uint) basicRating;
 
     function getBalance(address externalWallet) public view returns (uint balance) {
         return walletValue[externalWallet];
@@ -21,6 +22,6 @@ contract VirtualWallet {
     }
 
     function getRating(address ratingSystem) public view returns (uint rating) {
-        return 0;
+        return basicRating[ratingSystem];
     }
 }
