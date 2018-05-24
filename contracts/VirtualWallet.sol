3841974ed7e43c246c2806c7e53414b9d7799b6d
Guziec-Lobuziec <eth@eth.eth>
2018-05-24 16:13:30 +0200

    IS13 code - transfer

--- contracts/VirtualWallet.sol
+++ contracts/VirtualWallet.sol
@@ -17,6 +17,11 @@ contract VirtualWallet {
         require(walletValue[msg.sender] >= amount);
         walletValue[msg.sender] -= amount;
         msg.sender.transfer(amount);
+    }
 
+    function transfer(address to, uint256 value) public returns (bool success) {
+        require(getBalance(msg.sender) >= value, "Not enough assets");
+        walletValue[msg.sender] -= value;
+        walletValue[to] += value;
     }
 }
