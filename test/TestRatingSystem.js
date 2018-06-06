05485162250bc2b0ba80e810ecd4bb11329c052a
hentaininja <hasamt94@gmail.com>
2018-06-06 10:21:36 +0200

    IS3 test - final

--- test/TestRatingSystem.js
+++ test/TestRatingSystem.js
@@ -10,7 +10,7 @@ contract("VirtualRating Tests", async (accounts) => {
     it("test rank-presence", async () => {
         let testRating = 0;
 
-        assert.equal(testRank.getRating.call(accounts[0]), testRating, "Should be 0");
+        assert.equal(await(testRank.getRating.call(accounts[0])), testRating, "Should be 0");
     })
 
 })
